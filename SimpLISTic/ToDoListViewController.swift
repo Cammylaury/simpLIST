b0e8ca03643011cae0bd430a7624e79e127343d3
Cameron Laury <cammy.laury@gmail.com>
2018-02-21 11:53:00 -0600

    Add tableviews

---
+++ SimpLISTic/ToDoListViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ToDoListViewController.swift
+//  SimpLISTic
+//
+//  Created by Cameron Laury on 2/21/18.
+//  Copyright © 2018 Cameron Laury. All rights reserved.
+//
+
+import UIKit
+
+class ToDoListViewController: UITableViewController {
+    
+    let itemArray = ["Find Mike", "Buy Eggos", "Destroy Demogorgon"]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        
+        cell.textLabel?.text = itemArray[indexPath.row]
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print(indexPath.row)
+    }
+    
+
+}
+
